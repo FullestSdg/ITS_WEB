615c3b0e316f5209880054550c5149c4e8843734
FullestSdg <sdgsergiodg20@gmail.com>
2025-06-13 13:46:59 +0200

    updated

--- Lezione 11_06_2025/prima-app/src/Componente1.js
+++ Lezione 11_06_2025/prima-app/src/Componente1.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Componente1 = (props) => {
   return (
+    <>
     <div>Componente1 {props.children} </div>
+    <Anagrafica></Anagrafica>
+    <Messaggio></Messaggio>
+    </>
   )
 }
 
 export default Componente1
+
+const Anagrafica=()=> {
+
+    return (
+        <div> Anagrafica </div>
+    )
+
+}
+
+const Messaggio=()=> {
+
+    return (
+        <div> Messaggio </div>
+    )
+
+}
